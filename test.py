6fe26358eee01bf48279622cb1013347d1a9e17c
Top <top.talented@outlook.com>
2021-11-01 17:19:03 +0800

    Added filtering to method get_10_last_events

--- test.py
+++ test.py
@@ -30,7 +30,28 @@ def push_new_event(str):
 	events.insert(0, event)
 
 # Gets 10 last events from the stream
-# Params: None
+# Params:
+# category: category name to filter, values: "update", "poll", "warn", "", type: string
+# person: person name to filter, values: "all", "john", "all-friends", "", type: string
+# time: time value to filter, type: float
 # Returns the list of event object
-def get_10_last_events():
-	return events[:10]
+def get_10_last_events(category="", person="", time=0):
+	if category != "update" and category != "poll" and category != "warn" and category != "":
+		print("Invalid category")
+		return []
+	
+	if person != "all" and person != "john" and person != "all-friends" and person != "":
+		print("Invalid person")
+		return []
+
+	filtered = events
+	if category != "":
+		filtered = list( filter(lambda evt: (evt["category"] == category), filtered) )
+
+	if person != "":
+		filtered = list( filter(lambda evt: (evt["person"] == person), filtered) )
+
+	if time != 0:
+		filtered = list( filter(lambda evt: (evt["time"] >= time), filtered) )
+
+	return filtered[:10]
